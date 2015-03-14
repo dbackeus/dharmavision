d18ac8a21524a033ade8ba86a72186d949c8763a
David Backeus <duztdruid@gmail.com>
2015-03-15 00:05:52 +0100

    Movies scaffold

---
+++ app/models/movie.rb
@@ -0,0 +1,45 @@
+class Movie
+  include Mongoid::Document
+
+  field :title, type: String
+  field :imdb_id, type: String
+  field :plot, type: String
+  field :year, type: Integer
+  field :mpaa, type: String
+
+  validates_presence_of :title
+  validates_presence_of :imdb_id
+  validates_uniqueness_of :imdb_id
+  validate :validate_imdb_id, on: :create, if: :imdb_id
+
+  before_validation :set_attributes_from_imdb, on: :create
+
+  def imdb_id=(id_or_url)
+    if id_or_url =~ /tt(\d{1,})/
+      super($1)
+    else
+      super(id_or_url)
+    end
+  end
+
+  private
+  def imdb_movie
+    @imdb_movie ||= Imdb::Movie.new(imdb_id)
+  end
+
+  def set_attributes_from_imdb
+    self.title = imdb_movie.title
+    self.plot = imdb_movie.plot
+    self.year = imdb_movie.year
+    self.mpaa = imdb_movie.mpaa_rating
+  rescue OpenURI::HTTPError # This gets raised by Imdb::Movie when requesting year on nonexisting movie for unknown reason
+  end
+
+  def set_accepted_status
+    update_attribute(:accepted, true) if !accepted && ratings.count >= 5
+  end
+
+  def validate_imdb_id
+    errors[:imdb_id] << "needs to be a valid url" unless imdb_movie.title.present?
+  end
+end
---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+describe Movie do
+  it { is_expected.to validate_presence_of :title }
+  it { is_expected.to validate_presence_of :imdb_id }
+  it { is_expected.to validate_uniqueness_of :imdb_id }
+
+  describe "#imdb_id=" do
+    it "parses the imdb id from imdb urls if given a url" do
+      Movie.new(imdb_id: "0169102").imdb_id.should == "0169102"
+      Movie.new(imdb_id: "http://www.imdb.com/title/tt0169102/").imdb_id.should == "0169102"
+    end
+  end
+
+  context "being created" do
+    it "validates imdb ids to exist on imdb" do
+      movie = Movie.new(imdb_id: "0169102")
+      movie.save
+
+      movie.errors[:imdb_id].should_not include "needs to be a valid url"
+
+      movie = Movie.new(imdb_id: "234169102")
+      movie.save
+
+      movie.errors[:imdb_id].should include "needs to be a valid url"
+    end
+
+    it "assigns title from imdb" do
+      movie = Movie.create!(imdb_id: "0169102")
+      movie.title.should == "Lagaan: Once Upon a Time in India"
+      movie.plot.should == "The people of a small village in Victorian India stake their future on a game of cricket against their ruthless British rulers..."
+      movie.year.should == 2001
+      movie.mpaa.should == "Rated PG for language and some violence"
+    end
+  end
+end
