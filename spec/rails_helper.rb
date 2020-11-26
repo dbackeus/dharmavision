0d91d0a5963f19b21e1af838dad7d9c5424cf466
David Backeus <duztdruid@gmail.com>
2020-11-26 18:58:22 +0100

    Maintain test schema

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,15 @@ require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 require "webmock/rspec"
 
+# Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove these lines.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
