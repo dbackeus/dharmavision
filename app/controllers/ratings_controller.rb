48e99df10e40edc7e9c95c75f144f1a9204b078e
David Backeus <duztdruid@gmail.com>
2021-07-15 18:37:10 +0200

    Handle removing rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,11 +1,17 @@
 class RatingsController < ApplicationController
+  # Note: Sending rating 0 will remove rating
   def create
     rating_params = params.require(:rating)
     movie = Movie.find(rating_params[:movie_id])
 
     rating = movie.ratings.find_or_initialize_by(user: current_user)
     rating.rating = rating_params[:rating]
-    rating.save! # we're assuming client only sends valid ratings
+
+    if rating.rating == 0
+      rating.destroy
+    else
+      rating.save! # we're assuming client only sends valid ratings
+    end
 
     head :no_content
   end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -5,6 +5,7 @@ class Rating < ActiveRecord::Base
   validates_inclusion_of :rating, in: 1..10
 
   after_save :update_movie_average_rating
+  after_destroy :update_movie_average_rating
 
   # Ensure blank review values are saved as NULL
   def review=(review)
@@ -18,6 +19,6 @@ class Rating < ActiveRecord::Base
   private
 
   def update_movie_average_rating
-    movie.update! average_rating: movie.ratings.average(:rating)
+    movie.update! average_rating: movie.ratings.average(:rating) || 0
   end
 end
