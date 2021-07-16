6fc5c5baac4b9f52be525d326fdf2f5830331ffe
David Backeus <duztdruid@gmail.com>
2021-07-16 14:33:59 +0200

    Search keyboard nav

--- public/buildless/javascripts/movie_search.js
+++ public/buildless/javascripts/movie_search.js
@@ -1,15 +1,20 @@
 import debounce from "javascripts/utils/debounce"
 
 let resultList
+let selectedIndex
+let input
 
 document.addEventListener("turbolinks:load", () => {
   resultList = document.getElementById("movie-search-results")
 
-  const input = document.getElementById("movie-search")
+  input = document.getElementById("movie-search")
   input.addEventListener("input", debounce(onSearchInput, 200))
+  input.addEventListener("keydown", onKeyDown)
 })
 
 async function onSearchInput(e) {
+  selectedIndex = -1
+
   const query = e.target.value
   const html = await fetch(`/movies/search?query=${query}`, {
     headers: { 'X-Requested-With': 'XMLHttpRequest' }
@@ -17,6 +22,43 @@ async function onSearchInput(e) {
 
   resultList.style.display = html ? "block" : "none"
   resultList.innerHTML = html
+}
+
+function onKeyDown(e) {
+  switch (e.key) {
+    case "ArrowDown":
+      e.preventDefault()
+      selectedIndex++
+      select()
+      break
+    case "ArrowUp":
+      e.preventDefault()
+      selectedIndex--
+      select()
+      break
+    case "Enter":
+      const selectedListItem = resultList.querySelectorAll("li")[selectedIndex]
+      if(selectedListItem) {
+        e.preventDefault()
+        selectedListItem.querySelector("a").click()
+      }
+      break
+    case "Escape":
+      input.value = null
+      resultList.innerHTML = null
+  }
+}
+
+function select() {
+  const resultListItems = resultList.querySelectorAll("li")
+  if(resultListItems.length == 0) return
+
+  [...resultListItems].forEach(li => li.classList.remove("bg-blue-100"))
+
+  if(selectedIndex < 0) selectedIndex = resultListItems.length - 1
+  if(selectedIndex > resultListItems.length - 1) selectedIndex = 0
+
+  resultListItems[selectedIndex].classList.add("bg-blue-100")
 }
 
 export default "no export from movie search"
